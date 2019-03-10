2d133c9ab0941b9a425e14b18cc8216e03c5bc50
Kehinde Afolabi <afolabikehinde4@gmail.com>
2019-03-10 12:02:28 -0500

    initial controller and routes

---
+++ app/Http/Controllers/TicketsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TicketsController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,5 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'TicketsController@index');
+Route::get('/create', 'TicketsController@create');
